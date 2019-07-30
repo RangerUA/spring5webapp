44f5cd6f8c2aaec9af317a3ee7583fc92da8088e
Roman Kopylov <romanko@123software.ru>
2019-07-30 16:12:16 +0300

    some minor refactor

--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -2,11 +2,9 @@ package guru.springframework.spring5webapp.model;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
-/**
- * Created by jt on 5/16/17.
- */
 @Entity
 public class Book {
     @Id
@@ -14,84 +12,86 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
-    private String publisher;
-
+    
+    @OneToOne
+    private Publisher publisher;
+    
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
-    inverseJoinColumns = @JoinColumn(name = "author_id"))
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
-
+    
     public Book() {
     }
-
-    public Book(String title, String isbn, String publisher) {
+    
+    public Book(String title, String isbn, Publisher publisher) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
     }
-
-    public Book(String title, String isbn, String publisher, Set<Author> authors) {
+    
+    public Book(String title, String isbn, Publisher publisher, Set<Author> authors) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
         this.authors = authors;
     }
-
+    
     public Long getId() {
         return id;
     }
-
+    
     public void setId(Long id) {
         this.id = id;
     }
-
+    
     public String getTitle() {
         return title;
     }
-
+    
     public void setTitle(String title) {
         this.title = title;
     }
-
+    
     public String getIsbn() {
         return isbn;
     }
-
+    
     public void setIsbn(String isbn) {
         this.isbn = isbn;
     }
-
-    public String getPublisher() {
+    
+    public Publisher getPublisher() {
         return publisher;
     }
-
-    public void setPublisher(String publisher) {
+    
+    public void setPublisher(Publisher publisher) {
         this.publisher = publisher;
     }
-
+    
     public Set<Author> getAuthors() {
         return authors;
     }
-
+    
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
     }
-
+    
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-
+        
         Book book = (Book) o;
-
-        return id != null ? id.equals(book.id) : book.id == null;
+        
+        return Objects.equals(id, book.id);
     }
-
+    
     @Override
     public int hashCode() {
         return id != null ? id.hashCode() : 0;
     }
-
+    
     @Override
     public String toString() {
         return "Book{" +
